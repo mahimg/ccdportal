e5af17d9de13952341c38f3eebf9eebe25f17f33
Mahim Goyal <mahim97@gmail.com>
2018-11-30 14:03:29 +0530

    Netifaces

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import netifaces
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -86,6 +86,21 @@ DATABASES = {
         'PORT': '5433',
     }
 }
+
+# Find out what the IP addresses are at run time
+# This is necessary because otherwise Gunicorn will reject the connections
+def ip_addresses():
+    ip_list = ['localhost']
+    for interface in netifaces.interfaces():
+        addrs = netifaces.ifaddresses(interface)
+        for x in (netifaces.AF_INET, netifaces.AF_INET6):
+            if x in addrs:
+                ip_list.append(addrs[x][0]['addr'])
+    return ip_list
+
+
+# Discover our IP address
+ALLOWED_HOSTS = ip_addresses()
 
 
 # Internationalization
